c7603f6fd6f254da1702bbfe4d5870b5ef7b159e
Mateusz <traczyk.mateusz76@gmail.com>
2022-12-07 06:57:53 +0100

    lab 8

---
+++ Lab8/Assets/Scripts/Lab8/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float runSpeed = 30f;
+    bool jump = false;
+    float horizontalMove = 0f;
+
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+            Debug.Log("jump");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+
+    public void OnLanding()
+    {
+        Debug.Log("land");
+        animator.SetBool("IsJumping", false);
+    }
+}
